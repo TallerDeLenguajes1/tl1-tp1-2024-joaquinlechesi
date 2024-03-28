046acfd24b82731a8bfe1f5de2136fbf72b1b7e1
joaquinlechesi <joaquinlechesi@gmail.com>
2024-03-28 17:00:18 -0300

    segundo commit

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -2,6 +2,14 @@
 
 int main()
 {
+    int edad, *p_edad;
+    edad = 28;
+    p_edad = &edad;
     puts("Hola mundo");
+    printf("El puntero declara contiene: %d", p_edad);
+    printf("\nLa direccion de memoria almacenada por el puntero es: %p", p_edad);
+    printf("\nLa direccion de memoria de la variable es: %p", &edad);
+    printf("\nLa direccion de memoria del puntero es: %p", &p_edad);
+    printf("\nEl tamanio de memoria utilizado por la variable es: %zu", sizeof(int));
     return 0;
 }
